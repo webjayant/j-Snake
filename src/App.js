0c8600c402c9b0ff3fc90ed8b9b92ea456e51290
Jayant Raj SIngh <jayant.singh@flocareer.com>
2023-09-13 05:37:58 +0530

    login event fix

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import {useEffect, useState}  from 'react'
 import JSnake from "./components/jSnake";
 import Header from "./components/Header";
 import { loginUser, logoutUser } from "./utils/netlifyIdentity";
+import netlifyIdentity from 'netlify-identity-widget'
 import './App.css';
 
 
 function App() {
   const [currentUser, setCurrentUser] = useState()
-  useEffect(()=>{
-      const user = localStorage.getItem("currentUser");
+
+  netlifyIdentity.on('login', ()=>{
+     const user = localStorage.getItem("currentUser");
       console.log(user, 'usr')
       if (user) {
         setCurrentUser(JSON.parse(user))
       }
-  },[])
+  })
+  netlifyIdentity.on('logout', ()=>{
+       setCurrentUser(null)
+ })
   return(
     <>
       <Header handleLogin={loginUser} handleLogout={logoutUser} currentUser={currentUser}/>
