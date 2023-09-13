2b98f7a8d36c1afc46b387a0ecd61b67dcdf9d33
Jayant Raj SIngh <jayant.singh@flocareer.com>
2023-09-13 05:30:29 +0530

    added author and thumbnail

--- src/components/userDropdown.js
+++ src/components/userDropdown.js
@@ -14,11 +14,11 @@ const UserDropdown = ({currentUser, handleLogout}) => {
     return (
         <>
             <div className="userDetails" onClick={()=>handleClick()}>
-                <img src={currentUser.user_metadata.avatar_url} alt="" />
+                <img src={currentUser.avatar_url} alt="" />
             </div>
             <div className={`userDropdown ${isDDOpen?'open':null}`}>
-                <img src={currentUser.user_metadata.avatar_url} alt="" />
-                <p>Name:{currentUser.user_metadata.full_name}</p>
+                <img src={currentUser.avatar_url} alt="" />
+                <p>Name:{currentUser.full_name}</p>
                 <p>Highest Score: xxx</p>
                 <button className='btn linkBtn' onClick={()=>handleLogout()}>Logout</button>
                 <button className='btn linkBtn' onClick={()=>showLeaderBoard()}>Leader Board</button>
