267c5ab8933825256a3d08e811fd5dca47878ceb
Jayant Raj SIngh <jayant.singh@flocareer.com>
2023-09-13 05:44:37 +0530

    login event fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import UserDropdown from './userDropdown';
-const Header = ({handleLogin, handleLogout, currentUser}) => { 
-    const [isLoggedin, setIsLoggedin] = useState(false)
-    useEffect(()=>{
-        if(currentUser && currentUser.id){
-            setIsLoggedin(true)
-        }
-    },[currentUser])
+const Header = ({handleLogin, handleLogout, currentUser, isLoggedin}) => { 
     return (
         <div className='header'>
             <div className="logo">j~Snake</div>
