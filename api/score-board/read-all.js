570da1e17d5af2e5d949550162acf16b65c7d1d1
Jayant Raj SIngh <jayant.singh@flocareer.com>
2023-09-18 13:46:35 +0530

    fixed get all

--- api/score-board/read-all.js
+++ api/score-board/read-all.js
@@ -12,15 +12,9 @@ const handler = async () => {
 
   try {
     const response = await client.query(query.Match(query.Index('all_userScores')))
-    const itemRefs = response.data
-    // create new query out of item refs. http://bit.ly/2LG3MLg
-    const getAllItemsDataQuery = itemRefs.map((ref) => query.Get(ref))
-    // then query the refs
-    const ret = await client.query(getAllItemsDataQuery)
-    console.log(ret, 'RET')
     return {
       statusCode: 200,
-      body: JSON.stringify(ret),
+      body: JSON.stringify(response.data),
     }
   } catch (error) {
     console.log('error', error)
