fe53987d4064244aab8ce2353e0198235bfa6f77
Jayant Raj SIngh <jayant.singh@flocareer.com>
2023-09-19 05:21:15 +0530

    highScore fix

--- api/score-board/create.js
+++ api/score-board/create.js
@@ -1,41 +1,62 @@
-const process = require('process')
+const process = require('process');
 
-const { Client, query } = require('faunadb')
+const { Client, query } = require('faunadb');
 
 /* configure faunaDB Client with our secret */
 const client = new Client({
-  secret: process.env.FAUNADB_SERVER_SECRET,
-})
+    secret: process.env.FAUNADB_SERVER_SECRET,
+});
 
 /* export our lambda function as named "handler" export */
 const handler = async (event) => {
-  /* parse the string body into a useable JS object */
-  const data = JSON.parse(event.body)
-  console.log('Function `create` invoked', data)
-  const item = {
-    data: data,
-  }
-  /* construct the fauna query */
-    const response = await client.query(query.Get(query.Match(query.Index('user_Score_By_Email'), item.data.email)))
-    if(response.data && response.data.email === item.data.email){
-      const updateResponse = await client.query(query.Update(response.ref, item))
-      return {
-        statusCode: 200,
-        body: JSON.stringify(updateResponse),
-      }
-    }else if(response.description === 'Set not found.'){
-      const createResponse = await client.query(query.Create(query.Collection('userScores'), item))
-      return {
-        statusCode: 200,
-        body: JSON.stringify(createResponse),
-      }
-    }else{
-      return {
-        statusCode: 400,
-        body: JSON.stringify(response),
-      }
+    /* parse the string body into a useable JS object */
+    const data = JSON.parse(event.body);
+    console.log('Function `create` invoked', data);
+    const item = {
+        data: data,
+    };
+    /* construct the fauna query */
+    try {
+        const response = await client.query(
+            query.Get(
+                query.Match(query.Index('user_Score_By_Email'), item.data.email)
+            )
+        );
+        if (response.data && response.data.email === item.data.email) {
+            try {
+                const updateResponse = await client.query(
+                    query.Update(response.ref, item)
+                );
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(updateResponse),
+                };
+            } catch (error) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify(error),
+                };
+            }
+        }
+    } catch (error) {
+        if (error.errorType === 'NotFound') {
+            try {
+                const createResponse = await client.query(
+                    query.Create(query.Collection('userScores'), item)
+                );
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(createResponse),
+                };
+            } catch (error) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify(error),
+                };
+            }
+        }
     }
     /* Success! return the response with statusCode 200 */
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
